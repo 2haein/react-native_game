e3855e4f9ed46b44183516d7bcccce57cc1491ca
2haein <eehaein2@gmail.com>
2020-09-26 17:45:53 +0900

    publishing

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import DonkeyKong from "react-native-donkey-kong";
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
 
 // export default function App() {
 //   return (
@@ -12,8 +11,47 @@ import DonkeyKong from "react-native-donkey-kong";
 //   );
 // }
 
-export default class App extends React.Component {
+// export default class App extends React.Component {
+//   render() {
+//   }
+// }
+
+import React, { PureComponent } from "react";
+import { AppRegistry, StyleSheet, StatusBar } from "react-native";
+import { GameEngine } from "react-native-game-engine";
+import { Finger } from "./renderers";
+import { MoveFinger } from "./systems"
+ 
+export default class BestGameEver extends PureComponent {
+  constructor() {
+    super();
+  }
+ 
   render() {
-    return <DonkeyKong/>;
+    return (
+      <GameEngine
+        style={styles.container}
+        systems={[MoveFinger]}
+        entities={{
+          1: { position: [40,  200], renderer: <Finger />}, //-- Notice that each entity has a unique id (required)
+          2: { position: [100, 200], renderer: <Finger />}, //-- and a renderer property (optional). If no renderer
+          3: { position: [160, 200], renderer: <Finger />}, //-- is supplied with the entity - it won't get displayed.
+          4: { position: [220, 200], renderer: <Finger />},
+          5: { position: [280, 200], renderer: <Finger />}
+        }}>
+ 
+        <StatusBar hidden={true} />
+ 
+      </GameEngine>
+    );
   }
 }
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF"
+  }
+});
+ 
+AppRegistry.registerComponent("BestGameEver", () => BestGameEver);
